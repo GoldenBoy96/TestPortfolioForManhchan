b71be1801ac626f66c9abed4ebb9ee87bcf91273
Cậu vàng <huuminhgm@gmail.com>
2026-03-17 00:21:16 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
+const basePath = process.env.NODE_ENV === "production" ? "/TestPortfolioForManhchan" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
